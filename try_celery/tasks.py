bc8d23243153c6f1c9928624e6d022792fdeddd1
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-10-10 21:46:38 -0700

    send_task

--- try_celery/tasks.py
+++ try_celery/tasks.py
@@ -19,7 +19,8 @@ def task_one(start_time, source=None):
 def task_two():
     print("This is task two, now starting task one.")
     util()
-    task_one.delay(start_time=datetime.now(), source="task two")
+    # task_one.delay(start_time=datetime.now(), source="task two")
+    celery.current_app.send_task('try_celery.tasks.task_one', args=[datetime.now(), 'task two'])
 
 def util():
     print("Hey!")
