7a671c3ba08dc1c701bd54486aa3767aa5fa9736
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-10-10 21:40:24 -0700

    rate limit

--- try_celery/tasks.py
+++ try_celery/tasks.py
@@ -9,14 +9,16 @@ app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 
-@app.task
+@app.task(rate_limit="1/m")
 def task_one(start_time, source=None):
     print(f"This is task one, started at {start_time}, source {source}")
 
 
-@app.task
+
+@app.task(rate_limit="1/m")
 def task_two():
     print("This is task two, now starting task one.")
+    util()
     task_one.delay(start_time=datetime.now(), source="task two")
 
 def util():
